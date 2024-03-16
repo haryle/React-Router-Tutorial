2245f5d302910cc2589df1f73c881f16a07609cd
hoangson <lehoangsonsg@gmail.com>
2024-03-16 22:52:06 +1030

    feat: redirecting new record

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLoaderData, Form } from "react-router-dom";
+import { Link, Outlet, useLoaderData, Form, redirect } from "react-router-dom";
 import { getContacts, createContact } from "../contacts"
 
 export async function loader() {
@@ -8,7 +8,7 @@ export async function loader() {
 
 export async function action(){
     const contacts = await createContact();
-    return {contacts};
+    return redirect(`/contacts/${contacts.id}/edit`);
 }
 
 export default function Root() {
